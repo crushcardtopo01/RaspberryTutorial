a960c1838510370be43a4b5efea5a6047c78754a
Carlos Trevera Davila <silentlv5@gmail.com>
2020-02-17 09:43:50 -0600

    modificación

--- Humedad/humedad.py
+++ Humedad/humedad.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 '''
 Nombre: humedad.py 
 Creador: MICROSIDE TECHNOLOGY S.A de C.V / Carlos Trevera Davila  
@@ -6,59 +9,59 @@ Resumen: Codigo que conecta un sensor de Humedad del modulo XIDE
 		 por medio de GPIO en Raspberry.
 '''
 
-#Libreria para poder realizar interfaz grafica, 
+#Librer\nía para poder realizar interfaz grafica, 
 from Tkinter import * 
 
-# libreria para poder utilizar los GPIO de la raspberry pi                            
-import RPi.GPIO as GPIO
+#Librería para poder utilizar los GPIO de la raspberry pi                            
+#import RPi.GPIO as GPIO
 
-# librera para poder utilizar el puerto serial                   
+#Librería para poder utilizar el puerto serial                   
 import serial 
 
-# libreria para poder utilizar timers                                        
+#Librería para poder utilizar timers                                        
 import threading 
 
-# Libreria para controlar lapsos de tiempo                                   
+#Librería para controlar lapsos de tiempo                                   
 import time 
 
-#libreria para utilizacion de esquemas numericos                                                
+#librería para utilización de esquemas numéricos                                                
 import numpy as np                             
 
 
 '''
 Se crea la variable Raiz para tener una ventana
-donde viualizar los datos, el titulo sera "Humedad"
-y tendra un tamaño de 250 x 60 pixeles
+donde viualizar los datos, el título será "Humedad"
+y tendra un tamaño de 250 x 60 píxeles
 '''
 RAIZ = Tk()                                          
-RAIZ.title(“Humedad”)                          
-RAIZ.geometry(‘250×60’)                        
+RAIZ.title('Humedad')                          
+RAIZ.geometry(' 60×250')                        
 
 
 '''
-La variable puerto obtendra el puerto por el cual esta conectado el dispositivo 
-con su configuracion.
+La variable puerto obtendrá el puerto por el cual esta conectado el dispositivo 
+con su configuración.
 '''
-puerto= serial.Serial(port = ‘/dev/ttyS0’,                   
+puerto= serial.Serial(port = '/dev/ttyS0',                   
 baudrate = 9600,
 bytesize = serial.EIGHTBITS,
 parity = serial.PARITY_NONE,
 stopbits = serial.STOPBITS_ONE)
 
 dato = 0
-salto = “\\\\\r”
-orden=”H”
+salto = '\\\\\r'
+orden='H'
 
 '''
-Se crean todas los elementos que se mostrarán en la ventana,
+Se crean todos los elementos que se mostrarán en la ventana,
 la variable etiqueta sólo tendra el el texto "Valor de la humedad",
 la variable etiqueta1 cambiará su valor dependiendo del valor obtenido 
 del sensor.
 '''
-etiqueta = Label(RAIZ, text=”Valor de la Humedad es:”)
+etiqueta = Label(RAIZ, text='Valor de la Humedad es:')
 etiqueta.grid(column=4, row=5)
 
-etiqueta1 = Label(RAIZ, text=””)                          
+etiqueta1 = Label(RAIZ, text='')                          
 etiqueta1.grid(column=6, row=5)
 
 #Define en que tiempo y que función sera ejecutada con timer
@@ -66,7 +69,7 @@ timer = threading.Timer(1.1,fun)
 #Inicia el temporizador    
 timer.start()
 
-
+#Se inicia todo el programa 
 RAIZ.mainloop()
 
 #-------- Funciones a utilizar ------------
@@ -78,19 +81,19 @@ descripción: obtener y convertir los valores del sensor
 '''
 def fun():
     while True:
-       if (orden is “H”):
-       
-       puerto.write(orden.encode())
-       time.sleep(0.1)
-       puerto.write(salto.encode())
-       time.sleep(0.1)
-       dato = puerto.readline()
+       if (orden is "H"):
 
-       #convierte los valores para poder utilizarlos como enteros
-       dato = np.fromstring(dato.decode(‘ascii’, errors = ‘replase’), sep = ‘ ‘)   
+       	puerto.write(orden.encode())
+       	time.sleep(0.1)
+       	puerto.write(salto.encode())
+       	time.sleep(0.1)
+       	dato = puerto.readline()
 
-       #se muestra el valor en la ventana 
-       etiqueta1.config(text= dato[0])
+       	#convierte los valores para poder utilizarlos como enteros
+       	dato = np.fromstring(dato.decode('ascii', errors = 'replase'), sep = ' ')   
+
+       	#se muestra el valor en la ventana 
+       	etiqueta1.config(text= dato[0])
        #fin de  (if orden is "H")
     #fin while 
 # fin de la funcion fun 
